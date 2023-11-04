1ff17ef969290037c936ceef65adee5454332291
Jaswir Raghoe <jaswirraghoe@gmail.com>
2023-11-04 15:27:26 +0100

    Made basic send message + messages box component using existing library

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,34 +2,68 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isTyping, setIsTyping] = useState(false);
+
+  const [messages, setMessages] = useState([
+    // {
+    //   message: "Hello, I'm ChatGPT! Ask me anything!",
+    //   sentTime: "just now",
+    //   sender: "ChatGPT"
+    // }
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  const messageStyle = {
+    backgroundColor: 'darkred',
+    color: 'darksalmon',
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div style={{ position: "relative", height: "800px", width: "700px" }}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+              >
+                {messages.map((message, i) => {
+                  console.log(message)
+                  return <Message key={i} model={message} />
+                })}
+              </MessageList>
+              <MessageInput className="messageStyle" placeholder="Send a message" onSend={handleSend}
+                attachButton="false" />	
+            </ChatContainer>
+          </MainContainer>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
 export default App
+
+
