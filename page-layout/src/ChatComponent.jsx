3e950ed89890e0db0df84357b805f26c84d10f21
Zain Ul Abidin <zain26134@gmail.com>
2023-11-08 22:26:29 +0500

    Added OpenAI chat integration

--- page-layout/src/ChatComponent.jsx
+++ page-layout/src/ChatComponent.jsx
@@ -3,54 +3,56 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 import axios from 'axios'
 
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
 function ChatComponent() {
-    const [count, setCount] = useState(0)
+    
     const [isTyping, setIsTyping] = useState(false);
 
     const [messages, setMessages] = useState([
-        // {
-        //   message: "Hello, I'm ChatGPT! Ask me anything!",
-        //   sentTime: "just now",
-        //   sender: "ChatGPT"
-        // }
+        {
+          message: "Hello, I'm your Trading Assistant! How can i help you?",
+          direction: 'incoming',
+          sender: "TradePro"
+        }
     ]);
 
     const handleSend = async (message) => {
-        const newMessage = {
+        setMessages(oldMessages => [...oldMessages, {
             message,
             direction: 'outgoing',
             sender: "user"
-        };
+        }]);
 
-        const newMessages = [...messages, newMessage];
-
-        setMessages(newMessages);
-
-        // Initial system message to determine ChatGPT functionality
-        // How it responds, how it talks, etc.
         setIsTyping(true);
-        await processMessageToChatGPT(message);
+        processMessageToChatGPT(message).then((reply) => {
+            setIsTyping(false);
+            setMessages(oldMessages => [...oldMessages, {
+                message: reply,
+                direction: 'incoming',
+                sender: "TradePro"
+            }]);
+        });
     };
 
-    async function processMessageToChatGPT(message) {
-        axios.post('/mailtemplate', {
-            'text': this.getters['mailtemplate/getEditor'],
-            'subject': this.getters['mailtemplate/getSubject']
-        })
-            .then(response => {
-                $q.notify({
-                    color: 'dark',
-                    message: 'Mail template opgeslagen',
-                    position: 'top',
-                    timeout: 1000,
-                })
+    async function processMessageToChatGPT(message) 
+    {
+        const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${apiKey}` };
+        const payload = {
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": message }],
+            temperature: 0.7
+        };
 
-                logger.debug(response.data)
-
-            }).catch(err => {
-                logger.error(err)
-            })
+        try
+        {
+            const response = await axios.post('https://api.openai.com/v1/chat/completions', payload, { headers });
+            return response.data.choices[0].message.content.trim();
+        }
+        catch(error)
+        {
+            return error.message;
+        }
     }
 
     return (
@@ -60,10 +62,9 @@ function ChatComponent() {
                     <ChatContainer>
                         <MessageList
                             scrollBehavior="smooth"
-                            typingIndicator={isTyping ? <TypingIndicator content="SAFT-GPT is typing" /> : null}
+                            typingIndicator={isTyping ? <TypingIndicator content="TradePro is typing" /> : null}
                         >
                             {messages.map((message, i) => {
-                                console.log(message)
                                 return <Message key={i} model={message} />
                             })}
                         </MessageList>
